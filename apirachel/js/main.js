6018766f9a09702afd3b84f000c7f8cfc2110879
rae-2 <cloudandelion@gmail.com>
2023-12-13 12:45:20 -0500

    Update main.js

--- apirachel/js/main.js
+++ apirachel/js/main.js
@@ -9,7 +9,7 @@
     );
     let data = await res.json();
 
-    data.forEach((computer) => console.log(computer.calendar_year));
+    data.forEach((computer) => console.log(computer.oversight_agency, computer.full_location_address, computer.operating_status));
         
 }
 getData();

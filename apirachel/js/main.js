8eea2bd354e8a380e81f10b4c5d7430bdfb4b5fa
rae-2 <cloudandelion@gmail.com>
2023-12-20 12:45:53 -0500

    Update main.js

--- apirachel/js/main.js
+++ apirachel/js/main.js
@@ -7,7 +7,7 @@ import { DOMSelectors } from "./dom";
     let data = await res.json();
 
     data.forEach((computer) => console.log(computer.operating_status, computer.oversight_agency, computer.wheelchair_accessible, computer.full_location_address));
-
+//this is for the all computers button
     function makeCards(arr){
         DOMSelectors.results.innerHTML = "";
         arr.forEach((computer)=> {
@@ -15,9 +15,7 @@ import { DOMSelectors } from "./dom";
         newObject.innerHTML = 
         `
         <h2 class="place">${computer.full_location_address}</h2>
-        <h3 class="co">${computer.operating_status}</h3>
         <h3 class="agency">${computer.oversight_agency}</h3>
-        <h3 class="access">${computer.wheelchair_accessible}</h3>
         `
         newObject.result.add("card")
         DOMSelectors.results.appendChild(newObject)
@@ -30,8 +28,8 @@ getData();
 async function getData(){
 
 }
-
-
+//<h3 class="access">${computer.wheelchair_accessible}</h3>
+//<h3 class="co">${computer.operating_status}</h3>
 /* async function getData(URL){
     try{
         const response = await fetch(URL);

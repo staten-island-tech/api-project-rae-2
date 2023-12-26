a0a249ac95f83277e71c333dc9a8b7be0bfa7ecf
rae-2 <cloudandelion@gmail.com>
2023-12-26 12:53:43 -0500

    searchbar dont work

--- apirachel/js/main.js
+++ apirachel/js/main.js
@@ -1,24 +1,22 @@
-let API = `https://data.cityofnewyork.us/resource/sejx-2gn3.json?calendar_year=2023`;
-
 const newArr = []
 
 const DOMSelectors = {
-  every: document.querySelector(".all"),
-  yes: document.querySelector(".open"),
-  no: document.querySelector("close"),
-  hm: document.querySelector(".chair"),
-  idk: document.querySelector(".bar"),
-  output: document.querySelector("#results"),
-};
-
-async function getData(API) {
+    every: document.querySelector(".all"), //every public computer, should include computer.operating_status, computer.oversight_agency, computer.wheelchair_accessible, computer.full_location_address
+    yes: document.querySelector(".open"), //when this button is clicked, should show only computers that are open to the public
+    no: document.querySelector(".close"), //when this button is clicked, should show only computers that are closed
+    hm: document.querySelector(".chair"), //when this button is clicked, should show only computers that are wheelchair accessible
+    idk: document.querySelector(".bar"), //the search bar to manually search for a computer's location in one of the 5 boroughs
+    output: document.querySelector("#results"), //the div that prints out all the above data
+  };
+  
+  async function getData(API) {
     const api = "https://data.cityofnewyork.us/resource/sejx-2gn3.json?calendar_year=2023";
     
     try {
       const response = await fetch(api);
       
       if (!response.ok) {
-        throw new Error('erm erorr !!');
+        throw new Error('erm error !!');
       }
       
       const data = await response.json();
@@ -29,13 +27,52 @@ async function getData(API) {
   }
   
   function displayData(data) {
-    newArr.push(data);
-
-    data.forEach((computer) => console.log(computer.operating_status, computer.oversight_agency, computer.wheelchair_accessible, computer.full_location_address)); 
-  } 
+    // Clear previous data from the #results div
+    DOMSelectors.output.innerHTML = '';
   
-  getData(); 
+    data.forEach((computer) => {
+      const computerInfo = document.createElement('div');
+      
+      computerInfo.innerHTML = `
+        <p><strong>Operating Status:</strong> ${computer.operating_status}</p>
+        <p><strong>Oversight Agency:</strong> ${computer.oversight_agency}</p>
+        <p><strong>Wheelchair Accessible:</strong> ${computer.wheelchair_accessible}</p>
+        <p><strong>Full Location Address:</strong> ${computer.full_location_address}</p>
+        <hr>
+      `;
+      
+      // Append the computerInfo div to the #results div!!!
+      DOMSelectors.output.appendChild(computerInfo);
+    });
+  }
+  
+  getData();
 
+
+DOMSelectors.idk.addEventListener('input', function(event) {
+  const keyword = event.target.value.toLowerCase(); 
+  const filteredData = newArr[0].filter(computer => computer.full_location_address.toLowerCase().includes(keyword));
+
+  displayFilteredData(filteredData);
+});
+
+function displayFilteredData(filteredData) {
+  DOMSelectors.output.innerHTML = '';
+
+  filteredData.forEach((computer) => {
+    const computerInfo = document.createElement('div');
+    computerInfo.innerHTML = `
+      <p><strong>Operating Status:</strong> ${computer.operating_status}</p>
+      <p><strong>Oversight Agency:</strong> ${computer.oversight_agency}</p>
+      <p><strong>Wheelchair Accessible:</strong> ${computer.wheelchair_accessible}</p>
+      <p><strong>Full Location Address:</strong> ${computer.full_location_address}</p>
+      <hr>
+    `;
+    
+    // Append the computerInfo div to the #results div
+    DOMSelectors.output.appendChild(computerInfo);
+  });
+}
 
 
 //wheelchair and open or not

f5f531d8dd5a83f92f29cbe5fdc6e4b2566ab2fc
rae-2 <cloudandelion@gmail.com>
2024-01-02 18:42:53 -0500

    Update main.js

--- apirachel/js/main.js
+++ apirachel/js/main.js
@@ -2,9 +2,6 @@ let newArr = []
 
 const DOMSelectors = {
     every: document.querySelector(".all"),  //every public computer, should include computer.operating_status, computer.oversight_agency, computer.wheelchair_accessible, computer.full_location_address
-    yes: document.querySelector(".open"),  //when this button is clicked, should show only computers that are open to the public
-    no: document.querySelector(".close"),  //when this button is clicked, should show only computers that are closed
-    hm: document.querySelector(".chair"),  //when this button is clicked, should show only computers that are wheelchair accessible
     idk: document.querySelector(".bar"),   //the search bar to manually search for a computer's location from a keyword
     idk2: document.querySelector(".click"),   //once clicked, should show each computer's location in the keyword inputted into the search bar
     output: document.querySelector("#results"),   //the div that prints out all the above data
@@ -30,19 +27,19 @@ const DOMSelectors = {
   function displayData(data) {
     // Clear previous data from the #results div
     DOMSelectors.output.innerHTML = '';
-  
+    
     data.forEach((computer) => {
       const computerInfo = document.createElement('div');
       
       computerInfo.innerHTML = `
-      <p><strong>Operating Status:</strong> ${computer.operating_status}</p>
-      <p><strong>Oversight Agency:</strong> ${computer.oversight_agency}</p>
-      <p><strong>Wheelchair Accessible:</strong> ${computer.wheelchair_accessible}</p>
-      <p><strong>Full Location Address:</strong> ${computer.full_location_address}</p>
-      <hr>
+        <h3>Operating Status:</h3> ${computer.operating_status}<br>
+        <h3>Oversight Agency:</h3> ${computer.oversight_agency}<br>
+        <h3>Wheelchair Accessible:</h3> ${computer.wheelchair_accessible}<br>
+        <h3>Full Location Address:</h3> ${computer.full_location_address}<br>
+        <hr>
       `;
       
-      // Append the computerInfo div to the #results div!!!
+      // Append the computerInfo div to the #results div
       DOMSelectors.output.appendChild(computerInfo);
     });
   }
